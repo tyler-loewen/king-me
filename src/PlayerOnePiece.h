6aa1aed2999275aa25149a56b456b0de58b558d6
recb2720 <brandi.reck@gmail.com>
2015-04-15 23:13:33 -0600

    Added king images.

--- src/PlayerOnePiece.h
+++ src/PlayerOnePiece.h
@@ -9,12 +9,16 @@
 class PlayerOnePiece : public PieceInterface
 {
   public:
-     /**
+   /**
     ** Default constructor for player one pieces.
     */
-  PlayerOnePiece(unsigned char player1_r = 0, unsigned char player1_g = 0,
-		 unsigned char player1_b = 255);
-  
+   PlayerOnePiece(unsigned char player1_r = 0, unsigned char player1_g = 0,
+		  unsigned char player1_b = 255);
+   /**
+    * Destructs the PlayerOne Piece
+    */
+   ~PlayerOnePiece();
+   
    /**
     * Draws the game piece at the specified location.
     *
@@ -24,22 +28,28 @@ class PlayerOnePiece : public PieceInterface
     * @param width - The width of the board cell (in pixels).
     * @param height - The height of the board cell (in pixels).
     */
-  void drawPiece(bool is_king, unsigned int x, unsigned int y, unsigned int width, unsigned int height) const;
+   void drawPiece(bool is_king, unsigned int x, unsigned int y, unsigned int width, unsigned int height) const;
+  
   private:
-  /**
-   * Amount of red in the player one pieces.
-   */
-  unsigned char player1_red;
+   /**
+    * Amount of red in the player one pieces.
+    */
+   unsigned char player1_red;
 
-  /**
-   * Amount of green in the player one pieces.
-   */
-  unsigned char player1_green;
+   /**
+    * Amount of green in the player one pieces.
+    */
+   unsigned char player1_green;
 
-  /**
-   * Amount of blue in the player one pieces.
-   */
-  unsigned char player1_blue;
+   /**
+    * Amount of blue in the player one pieces.
+    */
+   unsigned char player1_blue;
+
+   /**
+    * Creates a bitmap to store the crown image
+    */
+   ALLEGRO_BITMAP *crown;
 };
 
 #endif
--- src/PlayerTwoPiece.cc
+++ src/PlayerTwoPiece.cc
@@ -4,7 +4,11 @@
 
 PlayerTwoPiece::PlayerTwoPiece(unsigned char player2_r, unsigned char player2_g,
 			       unsigned char player2_b)
-  : player2_red(player2_r), player2_green(player2_g), player2_blue(player2_b) {}
+   : player2_red(player2_r), player2_green(player2_g), player2_blue(player2_b)
+{
+   //Assigns the crown image
+   crown = al_load_bitmap("img/CrownIcon.png");
+}
 
 void PlayerTwoPiece::drawPiece(bool is_king, unsigned int x, unsigned int y, unsigned int width, unsigned int height) const
 {
@@ -14,4 +18,20 @@ void PlayerTwoPiece::drawPiece(bool is_king, unsigned int x, unsigned int y, uns
    al_draw_filled_ellipse(cx, cy, width / 2.5, height / 2.5, al_map_rgb(player2_red,
 									player2_green,
 									player2_blue));
+    if (is_king)
+   {
+      al_draw_scaled_bitmap(crown,
+			    0, 0,
+			    al_get_bitmap_width(crown),
+			    al_get_bitmap_height(crown),
+			    x + (width / 4), y + (height / 4),
+			    width / 2, height / 2,
+			    0);
+   }
+  
+}
+
+PlayerTwoPiece::~PlayerTwoPiece()
+{
+   al_destroy_bitmap(crown);
 }
